40edf88eeba00feca2ecb13a0d10e09d43c74124
nahtanpng <nathan.ferreiira.dev@gmail.com>
2025-06-07 21:56:39 -0300

    feat: update metadata for Nathan Ferreira's portfolio with enhanced descriptions and keywords

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,46 @@ const jetbrainsMono = JetBrains_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Nathan - Software Developer",
-  description: "Nathan Portfolio Page",
+  title: "Nathan Ferreira",
+  description:
+    "Portfólio de Nathan Ferreira, desenvolvedor de software especializado em aplicações web modernas, React, Next.js, e tecnologias de ponta. Veja projetos, experiências e entre em contato!",
+  keywords: [
+    "Nathan Ferreira",
+    "nahtanpng",
+    "Portfólio",
+    "Desenvolvedor",
+    "Software",
+    "React",
+    "Next.js",
+    "Nest.js",
+    "Backend",
+    "Frontend",
+    "Web Developer",
+    "Projetos",
+    "Experiência",
+  ],
+  openGraph: {
+    title: "Nathan Ferreira - Desenvolvedor de Software",
+    description:
+      "Portfólio de Nathan Ferreira, desenvolvedor de software especializado em aplicações web modernas, React, Next.js, e tecnologias de ponta.",
+    url: "https://nathanferreira.dev/",
+    siteName: "Nathan Ferreira Portfolio",
+    images: [
+      {
+        url: "/avatar.png",
+        width: 800,
+        height: 600,
+        alt: "Nathan Ferreira Avatar",
+      },
+    ],
+    locale: "pt_BR",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+  },
 };
 
 export default function RootLayout({
